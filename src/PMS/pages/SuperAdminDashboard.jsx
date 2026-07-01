ec143e47afe70209596a78cff5f02b2b47be72d7
LednuJ14 <jundelpatuasic03@gmail.com>
2026-07-01 21:26:48 +0800

    fix error

--- src/PMS/pages/SuperAdminDashboard.jsx
+++ src/PMS/pages/SuperAdminDashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import apiService from '../../services/api';
+import { apiService } from '../../services/api';
 
 const SuperAdminDashboard = () => {
   const [properties, setProperties] = useState([]);
